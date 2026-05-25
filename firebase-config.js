0cb2e379da23c4e89da84a3cc4f73c3e0f52f808
danah <aa95925551@gmail.com>
2026-05-25 23:57:03 +0900

    Add Firebase configuration to firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAY5OxPSGbHSqDAwpL6vC5iFA8t8e5iwu0",
+  authDomain: "danah-tarot.firebaseapp.com",
+  projectId: "danah-tarot",
+  storageBucket: "danah-tarot.firebasestorage.app",
+  messagingSenderId: "474070774406",
+  appId: "1:474070774406:web:32e71f9d617dde0ea55707",
+  measurementId: "G-VMNJTFV5YJ"
+};
